fca51babc850ec9cfc4c8867e13ba9d11dba01b2
Greg <Greg>
2017-07-30 19:48:06 -0500

    revised, working version

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -12,7 +12,7 @@ $(function() {
     $(document).on('click', 'a.page-scroll', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
+            scrollTop: $($anchor.attr('href')).offset().top - 100
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
